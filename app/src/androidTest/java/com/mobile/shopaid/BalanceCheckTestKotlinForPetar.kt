f64f93a6c432e854457723f3364033568a849336
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-08 20:18:50 +0200

    Converted test to kotlin

---
+++ app/src/androidTest/java/com/mobile/shopaid/BalanceCheckTestKotlinForPetar.kt
@@ -0,0 +1,46 @@
+package com.mobile.shopaid
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import com.mobile.shopaid.ui.activity.splash.SplashActivity
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BalanceCheckTestKotlinForPetar {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun saldoCheckTest() {
+        onView(allOf<View>(withId(R.id.entryInfoNextButton))).perform(click())
+        onView(withId(R.id.login_email)).perform(typeText("petarboy69@icemobile.com"))
+        onView(allOf<View>(withId(R.id.login_sign_in_button))).perform(click())
+        onView(withId(R.id.iban_iban_edittext)).perform(typeText("NL11INGB0008249866"))
+        onView(allOf<View>(withId(R.id.iban_next))).perform(click())
+        onView(allOf<View>(withId(R.id.cause_local))).perform(click())
+        Thread.sleep(2000)
+        onView(allOf<View>(withId(R.id.cause_international))).perform(click())
+        onView(allOf<View>(withId(R.id.cause_local))).perform(click())
+        onView(allOf<View>(withId(R.id.cause_next))).perform(click())
+        Thread.sleep(2000)
+        onView(allOf<View>(withId(R.id.partners_info_next))).perform(click())
+        onView(allOf<View>(withId(R.id.registration_complete_next))).perform(click())
+
+        // switch between tabs on balance page
+        Thread.sleep(2000)
+        onView(allOf<View>(withId(R.id.balance_detail_tab))).perform(click())
+        Thread.sleep(2000)
+        onView(allOf<View>(withId(R.id.balance_overview_tab))).perform(click())
+    }
+}
