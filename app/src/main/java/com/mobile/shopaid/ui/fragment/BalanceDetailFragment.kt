58249101a85e97667a73b41413f0eb3661a198a6
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-08 20:16:32 +0200

    Added balance detail adapter on balance detail fragment

--- app/src/main/java/com/mobile/shopaid/ui/fragment/BalanceDetailFragment.kt
+++ app/src/main/java/com/mobile/shopaid/ui/fragment/BalanceDetailFragment.kt
@@ -2,10 +2,13 @@ package com.mobile.shopaid.ui.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mobile.shopaid.R
+import kotlinx.android.synthetic.main.balance_detail_fragment.*
 
 
 class BalanceDetailFragment : Fragment() {
@@ -17,5 +20,33 @@ class BalanceDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        balance_detail_recyclerview.apply {
+
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@BalanceDetailFragment.activity)
+            adapter = BalanceDetailAdapter()
+        }
     }
+
+    class BalanceDetailAdapter :
+            RecyclerView.Adapter<BalanceDetailAdapter.ViewHolder>() {
+
+        // Each data item is just a string in this case that is shown in a TextView.
+        class ViewHolder(val textView: ViewGroup) : RecyclerView.ViewHolder(textView)
+
+        override fun onCreateViewHolder(parent: ViewGroup,
+                                        viewType: Int): BalanceDetailAdapter.ViewHolder {
+
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.balance_detail_list_row, parent, false) as ViewGroup
+
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//            holder.textView.text = myDataset[position]
+        }
+        override fun getItemCount() = 15
+    }
+
 }
--- app/src/main/java/com/mobile/shopaid/ui/fragment/CauseListFragment.kt
+++ app/src/main/java/com/mobile/shopaid/ui/fragment/CauseListFragment.kt
@@ -33,30 +33,21 @@ class CauseListFragment : Fragment() {
     class CauseAdapter :
             RecyclerView.Adapter<CauseAdapter.ViewHolder>() {
 
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder.
-        // Each data item is just a string in this case that is shown in a TextView.
+
         class ViewHolder(val textView: ViewGroup) : RecyclerView.ViewHolder(textView)
 
-        // Create new views (invoked by the layout manager)
         override fun onCreateViewHolder(parent: ViewGroup,
                                         viewType: Int): CauseAdapter.ViewHolder {
-            // create a new view
+
             val view = LayoutInflater.from(parent.context)
                     .inflate(R.layout.cause_list_row, parent, false) as ViewGroup
-            // set the view's size, margins, paddings and layout parameters
             return ViewHolder(view)
         }
 
-        // Replace the contents of a view (invoked by the layout manager)
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
 //            holder.textView.text = myDataset[position]
         }
 
-        // Return the size of your dataset (invoked by the layout manager)
         override fun getItemCount() = 15
     }
 
