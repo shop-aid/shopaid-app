a1a3c4f86465ca9a2d0fbea6b680574e355d1fb8
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-08 20:38:53 +0200

    Added some fields and population to causes adpater

--- app/src/main/java/com/mobile/shopaid/ui/fragment/CauseListFragment.kt
+++ app/src/main/java/com/mobile/shopaid/ui/fragment/CauseListFragment.kt
@@ -7,9 +7,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.mobile.shopaid.R
+import com.mobile.shopaid.data.response.CauseResponseModel
 import kotlinx.android.synthetic.main.cause_list_fragment.*
-
 
 class CauseListFragment : Fragment() {
 
@@ -20,35 +21,42 @@ class CauseListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         cause_recyclerview.apply {
 
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@CauseListFragment.activity)
-            adapter = CauseAdapter()
+            adapter = CauseAdapter(mutableListOf(
+                    CauseResponseModel("Test name", "Test description", true, true),
+                    CauseResponseModel("Test name222", "Test description222", true, true)
+            ))
         }
 
     }
 
-    class CauseAdapter :
+    class CauseAdapter(val causes : List<CauseResponseModel> ) :
             RecyclerView.Adapter<CauseAdapter.ViewHolder>() {
 
-
-        class ViewHolder(val textView: ViewGroup) : RecyclerView.ViewHolder(textView)
+        class ViewHolder(root: ViewGroup) : RecyclerView.ViewHolder(root) {
+            var name = root.findViewById(R.id.cause_name) as TextView
+            var description = root.findViewById(R.id.cause_description) as TextView
+        }
 
         override fun onCreateViewHolder(parent: ViewGroup,
                                         viewType: Int): CauseAdapter.ViewHolder {
 
             val view = LayoutInflater.from(parent.context)
                     .inflate(R.layout.cause_list_row, parent, false) as ViewGroup
+
             return ViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//            holder.textView.text = myDataset[position]
+            causes.get(position)
+            holder.name.text = causes.get(position).name
+            holder.description.text = causes.get(position).description
         }
 
-        override fun getItemCount() = 15
+        override fun getItemCount() = causes.size
     }
 
 }
