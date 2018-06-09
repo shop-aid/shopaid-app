77bef1fe9c9693b092048822a298b547b388d8b7
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-09 08:05:38 +0200

    Updated test to switch tabs

--- app/src/androidTest/java/com/mobile/shopaid/BalanceCheckTestKotlinForPetar.kt
+++ app/src/androidTest/java/com/mobile/shopaid/BalanceCheckTestKotlinForPetar.kt
@@ -1,14 +1,12 @@
 package com.mobile.shopaid
 
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.IdlingPolicies
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
@@ -38,21 +36,22 @@ class BalanceCheckTestKotlinForPetar {
         onView(allOf<View>(withId(R.id.login_sign_in_button))).perform(click())
         onView(withId(R.id.iban_iban_edittext)).perform(typeText("NL11INGB0008249866"))
         onView(allOf<View>(withId(R.id.iban_next))).perform(click())
-        onView(allOf<View>(withId(R.id.cause_local))).perform(click())
+
+        Thread.sleep(1000)
+        onView(allOf(withText("international"))).perform(click())
+        Thread.sleep(1000)
+        onView(allOf(withText("local"))).perform(click())
         onView(allOf(ViewMatchers.withId(R.id.cause_recyclerview), isCompletelyDisplayed()))
                 .perform(
                         RecyclerViewActions.scrollToPosition<CauseListFragment.CauseAdapter.ViewHolder>(5),
                         RecyclerViewActions.actionOnItemAtPosition<CauseListFragment.CauseAdapter.ViewHolder>(5, click()))
 
-        Thread.sleep(2000)
-//        onView(allOf<View>(withId(R.id.cause_international))).perform(click())
-//        onView(allOf<View>(withId(R.id.cause_local))).perform(click())
+        Thread.sleep(1000)
         onView(allOf<View>(withId(R.id.cause_next))).perform(click())
         Thread.sleep(5000)
         onView(allOf<View>(withId(R.id.partners_info_next))).perform(click())
         onView(allOf<View>(withId(R.id.registration_complete_next))).perform(click())
-
-        // switch between tabs on balance page
+        
         Thread.sleep(2000)
         onView(allOf<View>(withId(R.id.balance_detail_tab))).perform(click())
         Thread.sleep(2000)
--- app/src/main/java/com/mobile/shopaid/ui/activity/CauseActivity.kt
+++ app/src/main/java/com/mobile/shopaid/ui/activity/CauseActivity.kt
@@ -2,7 +2,6 @@ package com.mobile.shopaid.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
