cb5b8e190a56feb19aef2509533f555958536bf5
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-09 19:51:58 +0200

    Dynamic partner count

--- app/src/main/java/com/mobile/shopaid/ui/activity/PartnersActivity.kt
+++ app/src/main/java/com/mobile/shopaid/ui/activity/PartnersActivity.kt
@@ -54,7 +54,10 @@ class PartnersActivity : BaseActivity() {
     private fun initObservers() {
         partnerViewModel.partnerObservable.observe(this, Observer<ObservableResult<List<PartnerResponseModel>>> {
             when (it) {
-                is ObservableResult.Success -> partnerAdapter.partnerList = it.data
+                is ObservableResult.Success -> {
+                    partners_title.text = "The following ${it.data.size} stores joined ShopAid."
+                    partnerAdapter.partnerList = it.data
+                }
                 is ObservableResult.Error -> showError(it.exception.localizedMessage)
             }
         })
