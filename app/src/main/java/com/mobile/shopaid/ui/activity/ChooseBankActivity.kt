fe9f57f9fb0d73ce6353d8eed20ccc70dafe5743
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-09 18:32:46 +0200

    Empty axctiviteis

---
+++ app/src/main/java/com/mobile/shopaid/ui/activity/ChooseBankActivity.kt
@@ -0,0 +1,15 @@
+package com.mobile.shopaid.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mobile.shopaid.R
+
+class ChooseBankActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.choose_bank_activity)
+    }
+
+}
---
+++ app/src/main/java/com/mobile/shopaid/ui/activity/CredentialsActivity.kt
@@ -0,0 +1,15 @@
+package com.mobile.shopaid.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mobile.shopaid.R
+
+class CredentialsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.credentials_activity)
+    }
+
+}
