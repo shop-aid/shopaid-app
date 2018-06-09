d31cdb1e4d892c43a1db52f93d6c6c35dba42bb2
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-09 07:55:30 +0200

    Fixed test

--- app/src/androidTest/java/com/mobile/shopaid/BalanceCheckTestKotlinForPetar.kt
+++ app/src/androidTest/java/com/mobile/shopaid/BalanceCheckTestKotlinForPetar.kt
@@ -1,6 +1,8 @@
 package com.mobile.shopaid
 
+import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.IdlingPolicies
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.contrib.RecyclerViewActions
@@ -17,6 +19,7 @@ import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class BalanceCheckTestKotlinForPetar {
@@ -28,14 +31,18 @@ class BalanceCheckTestKotlinForPetar {
     @Test
     @Throws(InterruptedException::class)
     fun saldoCheckTest() {
+        IdlingPolicies.setMasterPolicyTimeout(15, TimeUnit.SECONDS)
+        Thread.sleep(2000)
         onView(allOf<View>(withId(R.id.entryInfoNextButton))).perform(click())
         onView(withId(R.id.login_email)).perform(typeText("petarboy69@icemobile.com"))
         onView(allOf<View>(withId(R.id.login_sign_in_button))).perform(click())
         onView(withId(R.id.iban_iban_edittext)).perform(typeText("NL11INGB0008249866"))
         onView(allOf<View>(withId(R.id.iban_next))).perform(click())
         onView(allOf<View>(withId(R.id.cause_local))).perform(click())
-        onView(allOf(ViewMatchers.withId(R.id.cause_recyclerview), isCompletelyDisplayed())).perform(
-                RecyclerViewActions.actionOnItemAtPosition<CauseListFragment.CauseAdapter.ViewHolder>(5, click()))
+        onView(allOf(ViewMatchers.withId(R.id.cause_recyclerview), isCompletelyDisplayed()))
+                .perform(
+                        RecyclerViewActions.scrollToPosition<CauseListFragment.CauseAdapter.ViewHolder>(5),
+                        RecyclerViewActions.actionOnItemAtPosition<CauseListFragment.CauseAdapter.ViewHolder>(5, click()))
 
         Thread.sleep(2000)
 //        onView(allOf<View>(withId(R.id.cause_international))).perform(click())
