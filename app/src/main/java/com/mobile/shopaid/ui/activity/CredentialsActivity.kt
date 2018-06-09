ed3244e78513efa04cc12981440e6949bf834bb0
Roderik Lagerweij <roderik.lagerweij@icemobile.com>
2018-06-09 18:41:22 +0200

    Styling for credentials page

--- app/src/main/java/com/mobile/shopaid/ui/activity/CredentialsActivity.kt
+++ app/src/main/java/com/mobile/shopaid/ui/activity/CredentialsActivity.kt
@@ -3,6 +3,7 @@ package com.mobile.shopaid.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mobile.shopaid.R
+import kotlinx.android.synthetic.main.credentials_activity.*
 
 class CredentialsActivity : AppCompatActivity() {
 
@@ -10,6 +11,10 @@ class CredentialsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.credentials_activity)
+
+        credentials_next.setOnClickListener({
+            // TODO
+        })
     }
 
 }
